ae4ed98c7b421118544e4f4d3b2c16ead4ddcdd0
real.overcomer.jc@gmail.com <real.overcomer.jc@gmail.com>
2021-10-02 20:34:22 +0200

    implemented create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import glob
 import os
@@ -7,6 +9,15 @@ import numpy as np
 
 from utils import get_module_logger
 
+def move_dataset_files(dataset_files, target_dir):
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    for file_path in dataset_files:
+        filename = os.path.basename(file_path)
+        target_path = os.path.join(target_dir, filename)
+        print('move to {}'.format(target_path))
+        os.rename(file_path, target_path)
 
 def split(data_dir):
     """
@@ -16,8 +27,31 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /mnt/data
     """
+
     # TODO: Implement function
-    
+    dataset_file_path = os.path.join(data_dir, '*.tfrecord')
+    dataset_files = glob.glob(dataset_file_path)
+    N = len(dataset_files)
+    #print('N = {}'.format(N))
+
+    n_train, n_valid, n_test = int(N*0.6), int(N*0.2), int(N*0.2)
+    #print('n_train = {}, n_valid = {}, n_test = {}'.format(n_train, n_valid, n_test))
+
+    train_files = dataset_files[:n_train]
+    valid_files = dataset_files[n_train:n_train+n_valid]
+    test_files = dataset_files[n_train+n_valid:]
+    #print('train_files = {}'.format(len(train_files)))
+    #print('valid_files = {}'.format(len(valid_files)))
+    #print('test_files = {}'.format(len(test_files)))
+
+    train_dir = os.path.join('.', 'data', 'train')
+    valid_dir = os.path.join('.', 'data', 'valid')
+    test_dir = os.path.join('.', 'data', 'test')
+
+    move_dataset_files(train_files, train_dir)
+    move_dataset_files(valid_files, valid_dir)
+    move_dataset_files(test_files, test_dir)
+
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
